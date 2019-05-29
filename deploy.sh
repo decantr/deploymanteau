6c65844e68fa99e62c0ccdd0020fff5f27743d80
decantr <theeasterlywood@gmail.com>
2019-05-29 01:35:35 +0100

    populate folders

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,21 @@
 #
 # tool to attempt to automate the deployment of
 # a static website
+
+repos="/srv/git"
+www="/srv/www"
+
+cd $www || exit 1
+
+stagit-index "$repos"/* > index.html
+
+for i in "$repos"/*; do
+	j=$(basename "$i" .git)
+
+	[ -d $j ] || mkdir $j
+	cd $j || exit 1
+
+	stagit $i
+
+	cd ..
+done
